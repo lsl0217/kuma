630c25848296770e47ae0aa16121d0e1aec32d96
lsl0217 <0217gir@gmail.com>
2024-01-04 10:51:12 +0900

    Initial commit

---
+++ src/com/ohgiraffers/test/run/Application.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.test.run;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("sdsd : ");
+        sc.nextLine();
+        System.out.println("ff");
+        sc.nextLine();
+
+
+
+
+
+
+    }
+}
---
+++ src/com/ohgiraffers/test/view/BookMenu.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.test.view;
+
+public class BookMenu {
+    int bNo;
+
+    int category;
+
+    String title;
+
+    String author;
+
+
+    
+
+}
